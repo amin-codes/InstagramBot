896ec7408c6d3cdbf0d5f71f35f2852be73776da
Amin Zamani <abz@email.unc.edu>
2020-12-24 02:17:00 -0500

    Initial upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# Instagram Messaging Bot
+This was just a fun, quick project I made to make a bot that can send custom messages to a specific user.
+
+Note: You can only message people that allow you to message them already.
+## Setup
+
+Make sure you have [Node.js](https://nodejs.org/) version >= v12.18.3 AND [Cypress](https://www.cypress.io/). You can install Cypress with the following command after installing Node:
+
+
+```bash
+npm install cypress
+```
+
+*Make sure you have set `chromeWebSecurity` to `false` in your `cypress.json` file. If you cloned this project directly, then you shouldn't have to worry about that. You can also always just copy my `cypress.json` file in this directory into yours*
+
+Then navigate to `cypress/integration/bot.spec.js` and modify the appropriate variables to set up your own user login, custom messages, and person you want to send those messages to. 
+
+## Usage
+
+After finishing the setup, from the project's root directory, run ANY of the following commands in your terminal (depending on whichever works for you):
+
+`'./node_modules/.bin/cypress' open`
+
+OR
+
+`./node_modules/.bin/cypress open`
+
+OR
+
+`cypress open`
+
+THEN click `bot.spec.js` from the menu and watch the bot do its thing.
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ cypress/integration/bot.spec.js
@@ -0,0 +1,32 @@
+context('Instagram Spammer', () => {
+
+    var insta_handle = 'HANDLE_OF_PERSON_YOU_WANT_TO_MESSAGE_WITHOUT_@'; //so if their handle is like @spongebob, just set the variable to 'spongebob'
+    let messages = ['(bot) get bot spammed', '(bot) hi', '(bot) idk some other random message', '(bot) ok ima stop now'];
+    var login_user = 'YOUR_USER_NAME_TO_LOGIN';
+    var login_pass = 'YOUR_PASSWORD_TO_LOGIN';
+
+    beforeEach(() => {
+        cy.visit('https://instagram.com/')
+    })
+    it('.type() - type into a DOM element', () => {
+        cy.get('form').within(() => {
+            cy.get('input[aria-label="Phone number, username, or email"]').type(login_user).should('have.value', login_user).then(() => {
+                cy.get('input[aria-label="Password"]').type(login_pass).should('have.value', login_pass).then(() => {
+                    cy.get('button[type="submit"]').click().then(() => {
+
+                    })
+                })
+            })
+        })
+        cy.wait(3000)
+        cy.visit('https://www.instagram.com/direct/inbox/')
+        cy.get('button').contains('Not Now').click()
+        cy.visit('https://www.instagram.com/'+insta_handle+'/')
+        cy.wait(3000)
+        cy.get('button[type="button"]').contains('Message').click()
+        for (var i=0; i<messages.length; i++) {
+            cy.get('textarea').eq(0).type(messages[i] + ' {enter}')
+            cy.wait(700)
+        }
+    })
+})
